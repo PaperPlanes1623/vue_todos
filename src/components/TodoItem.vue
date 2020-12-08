99b6fcf8f1bef156db0547a78f5422d3dbf1841b
PaperPlanes1623 <dylantaft16@gmail.com>
2020-12-07 20:51:19 -0700

    added event handling and styles

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,18 +1,27 @@
 <template>
-    <div>
-        <p>{{todo.title}}</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p> <input type="checkbox" v-on:click="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+        </p>
     </div>
 </template>
 
 <script>
 export default {
     name: "TodoItem",
-    props: ["todo"]
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
 }
 </script>
 
 <style scoped>
       .todo-item {
+          font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     background: #f4f4f4;
     padding: 10px;
     border-bottom: 1px #ccc dotted;
